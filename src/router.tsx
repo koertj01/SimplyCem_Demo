7b08c83c7458aec0bfaf864930e5bc04d3db4a28
Josh Koert <koertj01@gmail.com>
2024-12-29 22:41:54 -0500

    feature/Work_orders [Phase 1]
    * Added front end code to support a basic work order form.

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,11 @@
-import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/clerk-react";
 import { createBrowserRouter } from "react-router-dom";
-
 import Dashboard from "./components/Dashboard";
 import SignInPage from "./components/SignInPage";
-import MappingTools from "./components/Mapping/MappingTools";
+// import MappingTools from "./components/Mapping/MappingTools";
 import HomePage from "./components/Homepage";
 import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/clerk-react";
-import ProtectedLogin from "./components/ProtectedLogin";
-import { Box } from "@mui/material";
+// import ProtectedLogin from "./components/ProtectedLogin";
+// import { Box } from "@mui/material";
 import MappingPage from "./components/Mapping/MappingPage";
 import { WorkOrderForm } from "./components/WorkOrders/WorkOrderForm";
 
